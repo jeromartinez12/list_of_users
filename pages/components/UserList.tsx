5000614ef49c7b57d6eed941825f489e4fdb4825
Jeronimo <jeronimolmm@hotmail.com>
2023-07-25 19:52:30 -0700

    fix: some errors

---
+++ pages/components/UserList.tsx
@@ -0,0 +1,87 @@
+import { type User, SortBy } from "../types.d";
+
+interface UserListProps {
+  users: User[];
+  showColors: boolean;
+  deleteUser: (uuid: string) => void;
+  changeSorting: (sort: SortBy) => void;
+}
+
+export default function UserList({
+  users,
+  showColors,
+  deleteUser,
+  changeSorting,
+}: UserListProps) {
+  return (
+    <table className="divide-y divide-gray-800 text-center max-w-7xl">
+      <thead>
+        <tr className="leading-6 text-gray-900">
+          <th>Foto</th>
+          <th className="cursor-pointer" onClick={()=>changeSorting(SortBy.NAME)}>Nombre</th>
+          <th className="cursor-pointer" onClick={()=>changeSorting(SortBy.LAST)}>Apellido</th>
+          <th className="cursor-pointer" onClick={()=>changeSorting(SortBy.EMAIL)}>Correo</th>
+          <th className="cursor-pointer" onClick={()=>changeSorting(SortBy.COUNTRY)}>País</th>
+          <th>Acciones</th>
+        </tr>
+      </thead>
+
+      <tbody className="divide-y divide-gray-500 ">
+        {users.map((user, index) => {
+          const backgroundColor = index % 2 === 0 ? "#888" : "#999";
+          const color = showColors ? backgroundColor : "transparent";
+
+          return (
+            <tr
+              key={user.login.uuid}
+              className="min-w-0 flex-auto"
+              style={{ backgroundColor: color }}
+            >
+              <td>
+                <img
+                  className="h-12 w-12 flex-none rounded-full bg-gray-50"
+                  src={user.picture.thumbnail}
+                ></img>
+              </td>
+              <td className="text-sm font-semibold leading-6 text-gray-900">
+                {user.name.first}
+              </td>
+              <td className="text-sm font-semibold leading-6 text-gray-900">
+                {user.name.last}
+              </td>
+              <td className="text-sm font-semibold leading-6 text-gray-900">
+                {user.email}
+              </td>
+              <td className="text-sm font-semibold leading-6 text-gray-900">
+                {user.location.country}
+              </td>
+              <td>
+                <button
+                  onClick={() => {
+                    deleteUser(user.login.uuid);
+                  }}
+                >
+                  {" "}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="2"
+                    stroke="red"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,113 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
+import { useEffect, useMemo, useRef, useState } from "react";
+import { SortBy, type User } from "./types.d";
+import UserList from "./components/UserList";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [showColors, setShowColors] = useState(false);
+  const [sorting, setSorting] = useState<SortBy>(SortBy.NONE);
+  const [filterCountry, setFilterCountry] = useState<string | null>(null);
+
+  const originalUsers = useRef<User[]>([]);
+
+  const toogleColors = () => {
+    setShowColors(!showColors);
+  };
+
+  const toogleSortByCountry = () => {
+    const newSortingValue =
+      sorting == SortBy.NONE ? SortBy.COUNTRY : SortBy.NONE;
+    setSorting(newSortingValue);
+  };
+
+  const handleDelete = (uuid: string) => {
+    const filteredUsers = users.filter((user) => user.login.uuid !== uuid);
+    setUsers(filteredUsers);
+  };
+
+  const handleReset = () => {
+    setUsers(originalUsers.current);
+  };
+
+  const handleChangeSort = (sort: SortBy) => {
+    setSorting(sort);
+  };
+
+  useEffect(() => {
+    fetch("https://randomuser.me/api?results=100")
+      .then(async (res) => await res.json())
+      .then((res) => {
+        setUsers(res.results);
+        originalUsers.current = res.results;
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const filteredUsers = useMemo(() => {
+    return typeof filterCountry === "string" && filterCountry.length > 0
+      ? users.filter((user) => {
+          return user.location.country
+            .toLowerCase()
+            .includes(filterCountry.toLowerCase());
+        })
+      : users;
+  }, [filterCountry, users]);
+
+  const sortedUsers = useMemo(() => {
+    if (sorting === SortBy.NONE) return filteredUsers;
+
+    const comparePropierties: Record<string, (user: User) => any> = {
+      [SortBy.NAME]: (user) => user.name.first,
+      [SortBy.LAST]: (user) => user.name.last,
+      [SortBy.COUNTRY]: (user) => user.location.country,
+      [SortBy.EMAIL]: (user) => user.email,
+
+    };
+
+    return filteredUsers.toSorted((a, b) => {
+      const extractPropiertie = comparePropierties[sorting];
+      return extractPropiertie(a).localeCompare(extractPropiertie(b));
+    });
+  }, [filteredUsers, sorting]);
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <header className="flex flex-col justify-center items-center p-12">
+        <h1 className="text-4xl font-bold">Lista de usuarios</h1>
+        <div className="flex justify-center items-center p-4">
+          <button className="p-4" onClick={toogleColors}>
+            Colorear
+          </button>
+          <button className="p-4" onClick={toogleSortByCountry}>
+            {sorting === SortBy.COUNTRY
+              ? "No ordenar por país"
+              : "Ordenar por país"}
+          </button>
+          <button className="p-4" onClick={handleReset}>
+            Reset
+          </button>
+          <input
+            className="p-4"
+            placeholder="Filtrar por país"
+            onChange={(e) => {
+              setFilterCountry(e.target.value);
+            }}
+          />
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      </header>
+      <main
+        className={`flex flex-col items-center justify-center p-12 ${inter.className}`}
+      >
+        <UserList
+          changeSorting={handleChangeSort}
+          deleteUser={handleDelete}
+          showColors={showColors}
+          users={sortedUsers}
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      </main>
+    </>
+  );
 }
